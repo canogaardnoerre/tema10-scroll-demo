d380bd30f502d8764bebdcfee5f6cf4d578df453
canogaardnoerre <56019520+canogaardnoerre@users.noreply.github.com>
2020-10-09 15:19:54 +0200

    Merge

---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener('wheel', (event) => {
+    // event.deltaY;
+    let prodElement = document.querySelector('.demo-prod');
+    prodElement.style.cssText = "border: 10px solid pink";
+    
+});
+
+
+
+
